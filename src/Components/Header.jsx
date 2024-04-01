65d06eb361d58f3b18116d7f58b496dec5ba4a2f
Thirumurugan1234 <thirumurugan45984@gmail.com>
2024-04-01 19:04:24 +0530

    react commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import "./Header.css";
+import React from 'react'
+import './Header.css';
+import {FaUser,FaLock} from "react-icons/fa";
 
 export const Header = () => {
   return (
     <div className='wrapper'>
         <form action=''>
-            <h1>LOGIN FORM</h1>
+            <h1>LOGIN-FORM</h1>
             <div className='input-box'>
                 <input type='text' placeholder='username' required/>
+                <FaUser className='icon'/>
             </div>
             <div className='input-box'>
-                <input type='text' placeholder='Password' required/>
+                <input type='password' placeholder='password' required/>
+                <FaLock className='icon'/>
             </div>
             <div className='remember-forget'>
                 <label><input type='checkbox'/>Remember me</label>
+                <a href='#'>Forget Password</a>
+            </div>
+            <button type='submit'>Login</button>
+            <div className='register-link'>
+                <p>DON't have an account? <a href='#'>Register</a></p>
             </div>
         </form>
     </div>
-
   )
 }
