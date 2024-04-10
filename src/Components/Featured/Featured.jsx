e505c4c1fda25b6fb0fb3dd4f80861c638c79a9e
Thirumurugan1234 <thirumurugan45984@gmail.com>
2024-04-10 19:01:40 +0530

    commit a react

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -1,7 +1,11 @@
 
-import { CircularProgress } from "@mui/material";
+import { CircularProgressbar } from "react-circular-progressbar";
 import "./Featured.css"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
 
 export const Featured = () => {
   return (
@@ -12,10 +16,47 @@ export const Featured = () => {
             </div>
             <div className="bottom">
                 <div className="featuredChart">
-                    <CircularProgress value={70} text={"70%"}/>
+                    
+                    <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
 
         
             </div>
+            <p className="title">Total sale made today</p>
+            <p className="amount">$420</p>
+            <p className="desc">previous transaction processing,last payments may not be include.</p>
+            <div className="summary">
+                <div className="item">
+                    <div className="itemTitle">Target</div>
+                        <div className="itemResult negative" >
+                        <KeyboardArrowDownIcon fontSize="small" />
+
+                            <div className="resultAmount">$32.4k
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Last Week</div>
+                        <div className="itemResult positive" >
+                        <KeyboardArrowUpIcon fontSize="small" />
+
+                            <div className="resultAmount">$13.4k
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Last Month</div>
+                        <div className="itemResult positive" >
+                        <KeyboardArrowUpIcon fontSize="small" />
+
+                            <div className="resultAmount">$35.4k
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+    
         </div>
     </div>
   )
